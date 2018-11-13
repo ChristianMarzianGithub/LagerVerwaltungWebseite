e9fef1da45ac0e20840832109fc83beff6043fe8
ChristianMarzianGithub <cmarzian91@gmail.com>
2018-11-13 16:10:02 +0100

    showgoodliste bearbeitet

--- ClientApp/src/components/JS/LagerLogic/ShowGoodsListe.js
+++ ClientApp/src/components/JS/LagerLogic/ShowGoodsListe.js
@@ -1,27 +1,62 @@
 ﻿import React, { Component } from 'react';
 import $ from 'jquery';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 export class ShowGoodsListe extends Component{
 
     displayName = ShowGoodsListe.name
 
     constructor(props) {
+        debugger;
         super(props);
+        this.state = { warenDaten: [] };
+
+        method:'GET',
+        fetch('https://localhost:44323/api/LagerObjekt',
+            {
+
+                mode: 'no-cors'
+            }
+            ).then(data => {
+                this.setState({ warenDaten: data });
+            });
+    }
+
+
+
+
+
+    static getWarenDaten(warenDaten) {
+        return (<div>
+            <table className='table'  >
+                <thead>
+                    <tr>
+                        <th>Artikelbezeichnung</th>
+                        <th>Temp. (C)</th>
+                        <th>Temp. (F)</th>
+                        <th>Summary</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {warenDaten.map(warenDaten =>
+                        <tr key={warenDaten.id}>
+                            <td>{warenDaten.id}</td>
+                            <td>{warenDaten.bezeichnung}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+
+        </div>            );
     }
 
     render() {
+        ShowGoodsListe.getWarenDaten(this.state.warenDaten);
+
         return (
-                <div>
-                <p>ShowGoodsListe</p>
-                    <table className='table' >
-                        <th>Titel1</th><th>Titel2</th><th>Titel3</th>
-                    <tr>
-                        <td>eins</td><td>zwei</td><td>drei</td>
-                    </tr>
-                        <tr> <td>eins</td><td>zwei</td><td>drei</td>  </tr>
-                        <tr> <td>eins</td><td>zwei</td><td>drei</td>  </tr>
-                        <tr> <td>eins</td><td>zwei</td><td>drei</td>  </tr>
-                    </table>
+            <div>
+                {ShowGoodsListe.getWarenDaten(this.state.warenDaten)}
+
                 </div>            
             );
     }
