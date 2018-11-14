e1aaf8af8b01721e7383e1767a50729eabfad878
ChristianMarzianGithub <cmarzian91@gmail.com>
2018-11-14 15:58:00 +0100

    Drowdown-Component added
    for choosing LagerObjekt

---
+++ ClientApp/src/components/JS/LagerLogic/DropDownObjekt.js
@@ -0,0 +1,26 @@
+﻿import React, { Component } from 'react';
+import Select from 'react-select'; //https://react-select.com/home
+
+export class DropDownObjekt extends Component{
+
+    displayName = DropDownObjekt.name;
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const options = [
+            { value: 'chocolate', label: 'test' },
+            { value: 'strawberry', label: 'Strawberry' },
+            { value: 'vanilla', label: 'Vanilla' }
+        ];
+
+
+
+        return (
+            <div>
+                <Select options={options} />
+            </div>
+            
+            );
+    }
+}
