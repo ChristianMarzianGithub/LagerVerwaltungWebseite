ca10ce9e67b867647d580b9b2076b29ed7a5d78c
ChristianMarzianGithub <cmarzian91@gmail.com>
2018-11-14 15:58:52 +0100

    added Dropdown-Component

--- ClientApp/src/components/JS/LagerLogic/ShowGoods.js
+++ ClientApp/src/components/JS/LagerLogic/ShowGoods.js
@@ -1,19 +1,21 @@
 ﻿import React, { Component } from 'react';
+import 'react-dropdown/style.css';
 import { ShowGoodsListe } from './ShowGoodsListe';
 import { InsertGoods } from './InsertGoods';
-
+import { DropDownObjekt } from './DropDownObjekt';
 
 export class ShowGoods extends Component {
-    displayName = ShowGoods.name
+    displayName = ShowGoods.name;
 
     constructor(props) {
         super(props);
+        
     }
-
-
-    render() {
+    render() {      
+        
         return (
             <div>
+                <DropDownObjekt />
                 <InsertGoods />
                 <h1>Waren anzeigen</h1>
                 <ShowGoodsListe />
